0a620f3b4c88e9fbd731535c7986850bc82758b5
Albin Xavier Varghese <115107643+albinimpulze@users.noreply.github.com>
2024-02-12 17:43:43 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+# x = int(input("Enter the num "))
+# if((x%2)==0):
+#     print("Even")  
+# else:  
+#     print("Odd")
+
+# Task : Check the greatest of the two numbers, by taking the numbers as input from user
+# Task : Check the greatest of the three numbers, by taking the numbers as input from user
+    
+# Grading
+
+print("Hello" , (3+4))
+    
+x = 7
+x = x + 1
+x+=1
